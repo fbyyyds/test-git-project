b6568bb2ee3ec616615d1640da843f1e32758ee9
fbyyyds <1593190130@qq.com>
2023-09-29 01:40:45 +0800

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,14 @@
+/**
+ * @BelongsProject: test-git-project
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: fby
+ * @CreateTime: 2023-09-29  01:00
+ * @Description: TODO
+ * @Version: 1.0
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
